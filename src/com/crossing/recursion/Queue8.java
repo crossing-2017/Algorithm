d0b7c5247ac31244d2d181d25b25c4faf5242ecb
Crossing <crossing_2017@163.com>
2020-08-10 12:20:50 +0800

    完成迷宫问题

---
+++ src/com/crossing/recursion/Queue8.java
@@ -0,0 +1,14 @@
+package com.crossing.recursion;
+
+/**
+ * @author Crossing
+ * @Date 2020-08-10
+ * <p>
+ * 八皇后问题求解
+ */
+public class Queue8 {
+
+  public static void main(String[] args) {
+
+  }
+}
